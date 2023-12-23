3edef059917346103d113d2329ec1535b5f23611
Mario <mariojarapoblete@gmail.com>
2023-12-23 18:45:59 -0300

    Fin de seccion 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'base_16';
+  public title:string = 'Mi primera app Angular';
+
 }
